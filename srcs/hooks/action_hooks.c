41ea9961c594159ab7657d27cc6f72787b1d3949
Loïc <lbertran@student.42lyon.fr>
2021-06-18 12:44:51 +0200

    add arrows for movements

--- srcs/hooks/action_hooks.c
+++ srcs/hooks/action_hooks.c
@@ -6,7 +6,7 @@
 /*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/10 13:38:36 by lbertran          #+#    #+#             */
-/*   Updated: 2021/06/14 15:24:25 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/06/18 12:43:09 by lbertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,13 @@ int	handle_key_press(int keycode, t_view *view)
 {
 	if (keycode == 53)
 		exit(0);
-	else if (keycode == 13)
+	else if (keycode == 13 || keycode == 126)
 		handle_movement(view, view->player->x, view->player->y - 1);
-	else if (keycode == 0)
+	else if (keycode == 0 || keycode == 123)
 		handle_movement(view, view->player->x - 1, view->player->y);
-	else if (keycode == 1)
+	else if (keycode == 1 || keycode == 125)
 		handle_movement(view, view->player->x, view->player->y + 1);
-	else if (keycode == 2)
+	else if (keycode == 2 || keycode == 124)
 		handle_movement(view, view->player->x + 1, view->player->y);
 	return (0);
 }
